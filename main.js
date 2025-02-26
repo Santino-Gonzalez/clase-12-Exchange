e86865c6c159be5ceebf10dddb84e9d264aaa28e
Santino González <96146946+Santino-Gonzalez@users.noreply.github.com>
2025-02-26 11:18:17 -0300

    Corrijo error
    Se quita parte del código que generaba un error.

--- main.js
+++ main.js
@@ -2,9 +2,6 @@
 
 //Variables
 
-let base = $("#base");
-let fecha = $("#fecha");
-
 const simbolos = {
     "AUD": "A$",
     "BGN": "лв",
@@ -41,19 +38,19 @@ const simbolos = {
 //Agregar opciones de moneda base
 
 for (const moneda in simbolos) {
-    base.append($(`<option value="${moneda}">${moneda}</option>`));
+    $("#base").append($(`<option value="${moneda}">${moneda}</option>`));
 }
 
 //Seleccionar moneda base
 
 document.querySelector("#botonConfirmar").onclick = function () {
 
-    if (base[0].value !== "" && fecha[0].value !== "") {
+    if ($("#base")[0].value !== "" && $("#fecha")[0].value !== "") {
 
         //Mostrar Cotizaciones
 
         Promise.all([
-            fetch(`https://api.frankfurter.dev/v1/${fecha[0].value}?base=${base[0].value}`).then(res => res.json()),
+            fetch(`https://api.frankfurter.dev/v1/${$("#fecha")[0].value}?base=${$("#base")[0].value}`).then(res => res.json()),
             fetch(`https://api.frankfurter.dev/v1/currencies`).then(res => res.json())
         ])
             .then(([monedaJSON, nombresJSON]) => {
@@ -64,7 +61,7 @@ document.querySelector("#botonConfirmar").onclick = function () {
 
                 $("#containerCotizaciones").empty();
 
-                let base = base[0].value;
+                let base = $("#base")[0].value;
                 let cotizaciones = monedaJSON.rates;
                 cotizaciones[base] = 1;
 
@@ -72,8 +69,8 @@ document.querySelector("#botonConfirmar").onclick = function () {
                     const card = $("<div>", {
                         html: ` <h1>${moneda}</h1>
                                 <h2 id="nombreMoneda">${nombresJSON[moneda]}</h2>
-                                <p>1${simbolos[base[0].value]} = ${cotizaciones[moneda]} ${simbolos[moneda]}</p>
-                                <input min="0" placeholder="${base[0].value}" type="number" id="monedaBase">
+                                <p>1${simbolos[$("#base")[0].value]} = ${cotizaciones[moneda]} ${simbolos[moneda]}</p>
+                                <input min="0" placeholder="${$("#base")[0].value}" type="number" id="monedaBase">
                                 <input placeholder="${moneda}" type="number" id="cotizacion" disabled>
                                 <button class="botonConvertir">Convertir</button>`,
                         class: "cambio"
