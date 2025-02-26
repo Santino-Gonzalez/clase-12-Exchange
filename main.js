72d94c9ee5b7f352029a16c146f254445a16fd4e
Santino González <96146946+Santino-Gonzalez@users.noreply.github.com>
2025-02-26 10:32:41 -0300

    Selección de moneda y fecha
    Se permite al usuario seleccionar una moneda base, una fecha y se verifica que lo haga correctamente

--- main.js
+++ main.js
@@ -2,10 +2,59 @@
 
 //Variables
 
+let base = $("#base");
+let fecha = $("#fecha");
+
+const simbolos = {
+    "AUD": "A$",
+    "BGN": "лв",
+    "BRL": "R$",
+    "CAD": "C$",
+    "CHF": "₣",
+    "CNY": "¥",
+    "CZK": "Kč",
+    "DKK": "kr",
+    "GBP": "€",
+    "HKD": "£",
+    "HUF": "HK$",
+    "IDR": "Rp",
+    "ILS": "₪",
+    "INR": "₹",
+    "ISK": "kr",
+    "JPY": "¥",
+    "KRW": "₩",
+    "MXN": "$",
+    "MYR": "RM",
+    "NOK": "kr",
+    "NZD": "$",
+    "PHP": "₱",
+    "PLN": "zł",
+    "RON": "lei",
+    "SEK": "kr",
+    "SGD": "$",
+    "THB": "฿",
+    "TRY": "₺",
+    "USD": "$",
+    "ZAR": "R"
+}
+
 //Agregar opciones de moneda base
+
+for (const moneda in simbolos) {
+    base.append($(`<option value="${moneda}">${moneda}</option>`));
+}
 
 //Seleccionar moneda base
 
-//Mostrar Cotizaciones
+document.querySelector("#botonConfirmar").onclick = function () {
+
+    if (base[0].value !== "" && fecha[0].value !== "") {
+
+        //Mostrar Cotizaciones
+
+    } else {
+        alert("Debes seleccionar una moneda base y una fecha.");
+    }
+}
 
 // Convertir valores
